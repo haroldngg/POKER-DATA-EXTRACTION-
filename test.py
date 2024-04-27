da31761d95f3160f3d6356768a4cf98753468f77
Gusthavok <55331415+Gusthavok@users.noreply.github.com>
2024-04-27 04:57:15 +0200

    Delete test.py

--- test.py
+++
@@ -1,7 +0,0 @@
-from itertools import permutations
-
-# Générer toutes les permutations des éléments de 0 à 3
-permutations_list = list(permutations(range(4)))
-
-# Afficher les permutations
-print(permutations_list)
